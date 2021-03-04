ea54253ffe2868129ed075ad7d7840924ea04357
Sarah Taïlée <taileesarah@gmail.com>
2021-03-04 20:07:29 +0100

    changes from Sarah

--- book.py
+++ book.py
@@ -1,5 +1,27 @@
 class Order():
 
+    def __init__(self, quantity, price, buy=True): 
+        self.quantity=quantity
+        self.price=price
+        self.buy = buy 
 
-class Book():
+    def is_sell(self): 
+        return not self.buy
+
+    def __affichage__(self): 
+        return "Order(%s, %s)" % (self.quantity, self.price)
+  
+    
+   
+class Book(): 
+    
+    def __init__(self, name, sell=[], buy=[]):
+        self.name=name
+        self.sell=sell 
+        self.buy=buy
+
+
+    def insert_buy(): 
+
+        def insert_sell():
 
