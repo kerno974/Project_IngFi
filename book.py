f4cc4cbeb8cb2aa3c8614f9e0be068afa3690e7e
kerno974 <62987659+kerno974@users.noreply.github.com>
2021-03-23 10:08:02 +0100

    Update book.py

--- book.py
+++ book.py
@@ -1,27 +1,109 @@
-class Order():
-
-    def __init__(self, quantity, price, buy=True): 
-        self.quantity=quantity
-        self.price=price
-        self.buy = buy 
-
-    def is_sell(self): 
-        return not self.buy
-
-    def __affichage__(self): 
-        return "Order(%s, %s)" % (self.quantity, self.price)
-  
+class Book:
+    def __init__(self, Book_name):
+        self.Book_name = Book_name
+        self.BuyOrders = []
+        self.SellOrders = []
+        
+    def State_Book_BuySide(self):
+        if self.BuyOrders == [] :
+          print(" ")
+        else:
+            for i in range(len(self.BuyOrders)):
+                print("BUY ", self.BuyOrders[i].NbrAction,"@", self.BuyOrders[i].Price)
+            
+    def State_Book_SellSide(self):
+        if self.SellOrders == [] :
+          print(" ")
+        else:
+            for i in range(len(self.SellOrders)):
+                print("SELL ", self.SellOrders[i].NbrAction,"@", self.SellOrders[i].Price)
+                
+                
+    def log_display(self):
+        print("Book on " , self.Book_name, ':\n')
+        self.State_Book_BuySide()
+        self.State_Book_SellSide()
+        print("\n")
+        print("---------------------------------------------")
+        
+    def HighestBid(self):
+        print(self.BuyOrders[0].Price)
+        
+    def LowestAsk(self):
+        print(self.SellOrders[0].Price)
+        
+        
+    def insert_buy(self, NbrAction, Price):
+        order = Order(NbrAction, Price, 'Buy')
+        print("Insert BUY", order.NbrAction, "@", order.Price, "on", self.Book_name)
+        if self.BuyOrders == []:
+            self.BuyOrders.append(order)
+            self.log_display()
+        else:
+            for i in range(len(self.BuyOrders)):
+                if self.BuyOrders[i].Price < order.Price:
+                    self.BuyOrders.insert(i,order)
+                    self.log_display()
+                    break
+                #elif self.BuyOrders[i-1].Price == order.Price and self.BuyOrders[i].Price < order.Price :
+                elif self.BuyOrders[i].Price == order.Price:
+                    self.BuyOrders.insert(i+1,order)
+                    self.log_display()
+            if self.BuyOrders[-1].Price > order.Price :
+                self.BuyOrders.append(order)
+                self.log_display()
+        self.order_trades()
+            
+            
+    def insert_sell(self, NbrAction, Price):
+        order = Order(NbrAction, Price, 'Sell')
+        print("Insert SELL", order.NbrAction, "@", order.Price, "on", self.Book_name)
+        if self.SellOrders == []:
+            self.SellOrders.append(order)
+            self.log_display()
+        else:
+            for i in range(len(self.SellOrders)):
+                if self.SellOrders[i].Price > order.Price:
+                    self.SellOrders.insert(i, order)
+                    self.log_display()
+                    break
+                elif self.SellOrders[i].Price == order.Price and self.SellOrders[i+1].Price > order.Price :
+                    self.SellOrders.insert(i+1, order)
+                    self.log_display()
+            if self.SellOrders[-1].Price < order.Price:
+                self.SellOrders.append(order)
+                self.log_display()
+        self.order_trades()
+        
+    def order_trades(self):
+        for i in range(max(len(self.BuyOrders),len(self.SellOrders))):
+            if self.BuyOrders != [] and self.SellOrders != []:
+                if self.BuyOrders[0].Price >= self.SellOrders[0].Price:
+                    if self.BuyOrders[0].NbrAction == self.SellOrders[0].NbrAction:
+                        print("Execute ",self.BuyOrders[0].NbrAction, " at ", self.BuyOrders[0].Price , " on " , self.Book_name)
+                        del self.BuyOrder[0]
+                        del self.SellOrders[0]
+                       
+                        
+                    elif self.BuyOrders[0].NbrAction < self.SellOrders[0].NbrAction:
+                        print("Execute ",self.BuyOrders[0].NbrAction, " at ", self.BuyOrders[0].Price , " on " , self.Book_name)
+                        self.SellOrders[0].NbrAction = self.SellOrders[0].NbrAction - self.BuyOrders[0].NbrAction
+                        del self.BuyOrders[0]
+                      
+                    elif self.BuyOrders[0].NbrAction > self.SellOrders[0].NbrAction:
+                        print("Execute ",self.SellOrders[0].NbrAction, " at ", self.BuyOrders[0].Price , " on " , self.Book_name)                       
+                        self.BuyOrders[0].NbrAction= self.BuyOrders[0].NbrAction - self.SellOrders[0].NbrAction
+                        del self.SellOrders[0]
+                        
+                                      
+            
+        
+            
+            
+            
+class Order:
     
-   
-class Book(): 
-    
-    def __init__(self, name, sell=[], buy=[]):
-        self.name=name
-        self.sell=sell 
-        self.buy=buy
-
-
-    def insert_buy(): 
-
-        def insert_sell():
-
+    def __init__(self, NbrAction, Price, OrderType):
+        self.NbrAction = NbrAction
+        self.Price = Price
+        self.OrderType = OrderType
