6062336061ee2ae1a9c500bbd3d849141040b000
kerno974 <62987659+kerno974@users.noreply.github.com>
2021-03-31 13:15:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-
-if [[ ! -d /main/.env ]]
+pip3 install virtual env
+pip3 install --upgrade virtualenv
+if [[ ! -d ".env"]]
 then
-    python3 -m venv .env  
-    source .env/bin/activate
+    virtualenv -p python3 .env
 fi
-
 source .env/bin/activate
 pip3 install pandas
+pip3 install numpy
+
 python3 main.py
